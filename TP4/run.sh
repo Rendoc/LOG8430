52923ba2bc62ac41cd2fad9678e235446744b494
space-warf <space-warf@live.ca>
2018-12-02 22:57:20 +0000

    Fix run.sh

--- TP4/run.sh
+++ TP4/run.sh
@@ -4,6 +4,6 @@ apt-get install -y python-pip && \
 	apt-get install -y python2.7 && \
 	apt-get install -y mongodb
 service mongodb start
-pip --no-cache-dir install pyspark
-pip install -r application/requirements.txt
+# pip --no-cache-dir install pyspark
+pip --no-cache-dir install -r application/requirements.txt
 python2.7 application/app.py
