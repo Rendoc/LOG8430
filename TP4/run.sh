745ed5db9adbb197271d87c442a9e1329c5439cf
space-warf <space-warf@live.ca>
2018-12-02 22:55:51 +0000

    Modify run.sh to work with client.py

--- TP4/run.sh
+++ TP4/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt-get install -y python-pip
-apt-get install -y mongodb
+apt-get install -y python-pip && \
+	apt-get install -y python2.7 && \
+	apt-get install -y mongodb
 service mongodb start
+pip --no-cache-dir install pyspark
 pip install -r application/requirements.txt
 python2.7 application/app.py
