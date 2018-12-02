1753637f0ddbd4e0b0e00a2f6e76a7c9fdf0736e
api <space_warf@live.ca>
2018-12-02 23:13:44 +0000

    Add java to run.sh

--- TP4/run.sh
+++ TP4/run.sh
@@ -2,8 +2,8 @@
 
 apt-get install -y python-pip && \
 	apt-get install -y python2.7 && \
-	apt-get install -y mongodb
+	apt-get install -y mongodb && \
+	apt-get install -y openjdk-8-jre-headless
 service mongodb start
-# pip --no-cache-dir install pyspark
 pip --no-cache-dir install -r application/requirements.txt
 python2.7 application/app.py
